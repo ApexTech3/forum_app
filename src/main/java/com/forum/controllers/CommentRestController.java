33b6f90f91e67bacc7cdd7f3b62f893b9b0922de
andreev5r <andreev.petar.biz@gmail.com>
2024-01-23 13:11:57 +0200

    Created base RestControllers for tag and comment

---
+++ src/main/java/com/forum/controllers/CommentRestController.java
@@ -0,0 +1,30 @@
+package com.forum.controllers;
+
+import com.forum.helpers.AuthenticationHelper;
+import com.forum.models.Comment;
+import com.forum.services.CommentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/comments")
+public class CommentRestController {
+
+    private final CommentService service;
+    private final AuthenticationHelper authenticationHelper;
+
+    @Autowired
+    public CommentRestController(CommentService service, AuthenticationHelper authenticationHelper) {
+        this.service = service;
+        this.authenticationHelper = authenticationHelper;
+    }
+
+    @GetMapping
+    public List<Comment> get(){
+        return service.get();
+    }
+}
