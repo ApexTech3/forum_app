a18466a87a082d097e3d061c5603912f2e2e2414
andreev5r <andreev.petar.biz@gmail.com>
2024-01-23 13:11:39 +0200

    Copied AuthenticationHelper and created base for User so the other models can be built and serviced

---
+++ src/main/java/com/forum/services/UserService.java
@@ -0,0 +1,8 @@
+package com.forum.services;
+
+import com.forum.models.User;
+
+public interface UserService {
+
+    User get(String name);
+}
---
+++ src/main/java/com/forum/services/UserServiceImpl.java
@@ -0,0 +1,19 @@
+package com.forum.services;
+
+import com.forum.models.User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+
+
+    public UserServiceImpl(){}
+
+
+    @Override
+    public User get(String name) {
+        return null;
+    }
+}
+
