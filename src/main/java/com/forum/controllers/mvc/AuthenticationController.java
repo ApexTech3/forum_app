6952e56b945a9b4b98b4f340221587d167b0f294
Ivaylo37 <ivailo.vassilev99@gmail.com>
2024-02-07 02:45:10 +0200

    Finished the login template

--- src/main/java/com/forum/controllers/mvc/AuthenticationController.java
+++ src/main/java/com/forum/controllers/mvc/AuthenticationController.java
@@ -1,20 +1,50 @@
 package com.forum.controllers.mvc;
 
 import com.company.web.springdemo.models.LoginDto;
+import com.forum.exceptions.AuthenticationFailureException;
+import com.forum.helpers.AuthenticationHelper;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/auth")
 public class AuthenticationController {
 
+    private final AuthenticationHelper authenticationHelper;
+
+    public AuthenticationController(AuthenticationHelper authenticationHelper) {
+        this.authenticationHelper = authenticationHelper;
+    }
 
 
     @GetMapping("/login")
     public String showLoginPage(Model model) {
         model.addAttribute("login", new LoginDto());
         return "login";
+    }
+
+    @PostMapping("/login")
+    String handeLogin(@Valid @ModelAttribute("login") LoginDto dto,
+                      BindingResult bindingResult,
+                      HttpSession session) {
+        if (bindingResult.hasErrors()) {
+            return "login";
+        }
+
+        try {
+            authenticationHelper.verifyAuthentication(dto.getUsername(), dto.getPassword());
+            session.setAttribute("currentUser", dto.getUsername());
+            return "redirect:/";
+        } catch (AuthenticationFailureException e) {
+            bindingResult.rejectValue("username", "auth_error", e.getMessage());
+            return "login";
+        }
     }
 }
